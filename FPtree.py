1d8d461895841af5372c67b71258d5f97dea0dfc
Gerald Wu <wukakit96@gmail.com>
2021-03-31 10:45:36 +0800

    test successfully with sampled dataset

--- FPtree.py
+++ FPtree.py
@@ -57,20 +57,28 @@ class FPTree:
             self.add_record(record)
         return self.root
 
-    def merge(self, node_a, node_b):
+    def merge(self, node_a, node_b, tree=None):
         """
         merge node_a to node_b
         :param node_a: node
         :param node_b: node
         :return: new node_b
         """
+
+        tree = tree if tree else self
         node_b.count += node_a.count
         for offspring in sorted(node_a.next):
             if offspring not in node_b.next:
                 node_b.next[offspring] = node_a.next[offspring]
+                node_a.next[offspring].parent = node_b
             else:
-                node_b.next[offspring] = self.merge(node_a.next[offspring], node_b.next[offspring])
-        node_a.parent.next[node_b.name] = node_b
+                merge_next = self.merge(node_a.next[offspring], node_b.next[offspring], tree=tree)
+                node_b.next[offspring] = merge_next
+                merge_next.parent = node_b
+
+        # node_a.parent.next[node_b.name] = node_b  # node_a will be merged to node_b when node_a is del
+
+        tree.item_list[node_a.name].remove(node_a)
         del node_a
 
         return node_b
@@ -101,7 +109,7 @@ class FPTree:
                 # attach all offspring to node
                 for offspring in son.next:
                     if offspring in node.next:
-                        node.next[offspring] = self.merge(son.next[offspring], node.next[offspring])
+                        node.next[offspring] = self.merge(son.next[offspring], node.next[offspring], tree=tree)
                     else:
                         son.next[offspring].parent = node
                         node.next[offspring] = son.next[offspring]
@@ -115,29 +123,29 @@ class FPTree:
             # could be deleted when filtering other nodes
             if key in tree.support_list:
                 del tree.support_list[key]
-                print(key)
-                print(tree.support_list)
             if key in tree.item_list:
                 del tree.item_list[key]
-                print(key)
 
 
         return filtered, node
 
-    def check_empty(self, node):
+    def check_empty(self, node, tree=None):
         """
         delete all nodes with count==0 under a node
         :param node:
         :return:
         """
+
+        tree = tree if tree else self
         to_delete = []
         for key in node.next:
             if node.next[key].count == 0:
                 to_delete.append(key)
             else:
-                self.check_empty(node.next[key])
+                self.check_empty(node.next[key], tree=tree)
 
         for key in to_delete:
+
             del node.next[key]
 
         return node
@@ -150,93 +158,96 @@ class FPTree:
         :param min_support: minimum support to be a frequent item
         :return:
         """
-
-        print(f'\n\n--------- {to_cut} -----------')
-        if to_cut not in tree.support_list:
-            print(f'{to_cut} not in support list')
-        else:
-            print(f'support of {to_cut} is {tree.support_list[to_cut]}')  # FIXME: nodes bigger than to_cut are not supposed to exist
-        if to_cut not in tree.item_list:
-            print(f'{to_cut} not in item_list')
-        else:
-            print(f'item is {tree.item_list[to_cut]}')
+        # if to_cut == 52:
+        #     raise ValueError('52 check point')
+        # print(f'\n\n--------- {to_cut} -----------')
+        # if to_cut not in tree.support_list:
+        #     print(f'{to_cut} not in support list')
+        # else:
+        #     print(f'support of {to_cut} is {tree.support_list[to_cut]}')
+        # if to_cut not in tree.item_list:
+        #     print(f'{to_cut} not in item_list')
+        # else:
+        #     print(f'item is {tree.item_list[to_cut]}')
 
         min_support = min_support if min_support else self.min_support
 
-        print(f"0 in support ?: {0 in tree.support_list}")
+        # print(f"0 in support ?: {0 in tree.support_list}")
         # create conditional FP-tree
         cond_tree = deepcopy(tree)
+        # try:
+        #     print(f"new support_list: {cond_tree.support_list}")
+        #     print(f"new item_list 16: {cond_tree.item_list[16]}")
+        #     print(f"new root.next[16]: {cond_tree.root.next[16]}")
+        #     print(f"new tree root is {cond_tree.root}")
+        #
+        #     print(f"new nodes points to new tree?: {cond_tree.item_list[16][0].parent == cond_tree.root}")
+        # except:
+        #     pass
+
         cond_tree.support_list = defaultdict(int)
         cond_tree.item_list = defaultdict(list)
         # cond_tree.root = deepcopy(tree.root)
-        print("\n\n tree, and cond_tree's next")
-        try:
-            print("tree's next's parent")
-            print(tree.root.next[16].next[22].next[52].next[84].next)
-            print(f"original tree's support list {tree.support_list}")
-            print(f"original tree's item list {tree.item_list[16]}")
-            print(cond_tree.root.next[16].parent.name)
-            print("------------------")
-        except:
-            pass
-        try:
-            print(f"check 84 count: {cond_tree.root.next[16].count}")
-        except:
-            pass
+        # print("\n\n tree, and cond_tree's next")
+        # try:
+        #     # print("tree's next's parent")
+        #     # print(tree.root.next[16].next[22].next[52].next[84].next)
+        #     # print(f"original tree's support list {tree.support_list}")
+        #     # print(f"original tree's item list {tree.item_list[16]}")
+        #     # print(cond_tree.root.next[16].parent.name)
+        #     # print("------------------")
+        # except:
+        #     pass
+        # try:
+        #     print(f"check 84 count: {cond_tree.root.next[16].count}")
+        # except:
+        #     pass
 
         # clean count of conditional FP-tree
         cache = [cond_tree.root]
-        try:
-            print(f'cache next is {cache[0].next[16].parent.name}')  # 16's parent is not 2 here(_root)
-        except:
-            pass
 
-        # FIXME: a 2 and a 0 node are generated here
         while cache:
             # print(f"cache is: {[n.name for n in cache]}")
-            if to_cut == 84:
-                print(f"cache overview: ")
-                print(cache)
+            # if to_cut == 84:
+            #     print(f"cache overview: ")
+            #     print(cache)
             to_clean = cache.pop(0)
 
-            print(f"\nto clean is {to_clean}")
+            # print(f"\nto clean is {to_clean}")
             # print(to_clean)
             if to_clean.name != to_cut:
                 to_clean.count = 0
-                print(f'meet {to_clean.name} in clean')
-                try:
-                    print(f"    parent is {to_clean.parent.name}")
-                except:
-                    pass
+                # print(f'meet {to_clean.name} in clean')
+                # try:
+                #     print(f"    parent is {to_clean.parent.name}")
+                # except:
+                #     pass
             else:
-                print(f'    find {to_cut} in clean')
+                # print(f'    find {to_cut} in clean')
 
                 cond_tree.item_list[to_cut].append(to_clean)
-                try:
-                    print(f"    check append: {to_clean.parent.parent.parent.parent.name}")
-                except:
-                    pass
-            print(f"\n    {to_clean.name}'s next:")
+                # try:
+                #     print(f"    check append: {to_clean.parent.parent.parent.parent.name}")
+                # except:
+                #     pass
+            # print(f"\n    {to_clean.name}'s next:")
             for key in to_clean.next:
-                print(f'    there is next: {key}')
+                # print(f'    there is next: {key}')
                 cache.append(to_clean.next[key])
 
-            if to_cut == 84:
-                print("    -------\n    it's 84, go check\n    --------")
-                node = deepcopy(to_clean)
-                while node.name != '_root':
-                    print(f"    {node.name,}")
-                    print(f"    {node}")
-                    node = node.parent
-                print("    -------------------\n")
-
-        # FIXME: support not assigned here
-        try:
-            print(f'\n\nbefore for loop: {cond_tree.item_list[84][0].parent.parent.parent.parent.name} appears\n')
-            # FIXME: 2 is found here, 16's parent is supposed to be _root
-            # leaf nodes are used to retrieve other nodes bottom-up
-        except:
-            pass
+            # try:
+            #     if to_cut == 84:
+            #         print("    -------\n    it's 84, go check\n    --------")
+            #         li = [to_clean]
+            #         while li[-1].name != '_root':
+            #             node = li[-1]
+            #             print(f"    {node.name,}")
+            #             print(f"    {node}")
+            #             li.append(node.parent)
+            #         print("    -------------------\n")
+            #
+            # except:
+            #     pass
 
         # update count of nodes along all paths ending with to_cut to conditional FP-tree
         for node in cond_tree.item_list[to_cut]:
@@ -263,7 +274,7 @@ class FPTree:
             # attach to new cond_tree
             node.parent = cond_tree.root
             cond_tree.root.next[node.name] = node  # not repetitive
-        print(f'cut {to_cut} finished.')
+        # print(f'cut {to_cut} finished.')
 
         # delete to_cut
         for node in cond_tree.item_list[to_cut]:
@@ -271,14 +282,14 @@ class FPTree:
         del cond_tree.item_list[to_cut]
         del cond_tree.support_list[to_cut]
 
-        print(f"after cut, 0 is supported: {0 in cond_tree.support_list}")
+        # print(f"after cut, 0 is supported: {0 in cond_tree.support_list}")
         # clean empty-count nodes
-        cond_tree.root = self.check_empty(cond_tree.root)
+        cond_tree.root = self.check_empty(cond_tree.root, tree=cond_tree)
 
         # filter unsupported nodes
         cond_tree.filter_unsupported(cond_tree.root, cond_tree)
 
-        print(f"what's left: {cond_tree.root.next}")
+        # print(f"what's left: {cond_tree.root.next}")
 
         return cond_tree
 
@@ -294,7 +305,7 @@ class FPTree:
         for key in sorted([k for k in tree.support_list if k != '_root'], reverse=True):
             if tree.support_list[key] >= tree.min_support:  # if key is supported
                 freq_items.append(str(key) + endwith)  # cond_tree is None
-                print(tree.support_list)
+                # print(tree.support_list)
                 tree_cp = deepcopy(tree)
                 cond_tree = tree_cp.cut_tree(key, tree_cp)
 
@@ -307,13 +318,10 @@ if __name__ == "__main__":
     data_file = open('./data.pkl', 'rb')
     dataset = pickle.load(data_file)
 
-
-
-
     testset = [[1, 2, 3, 4],
                [1, 2, 3, 5],
-               [1, 2, 4, 6],
                [1, 2, 4, 5, 6],
+               [1, 2, 4, 6],
                [1, 2, 5],
                [1, 2, 5, 6],
                [1, 2, 6],
@@ -323,7 +331,9 @@ if __name__ == "__main__":
                [3, 4, 6],
                [3, 5],
                [6]]
-    tree = FPTree(dataset, 7)
+
+    samp_dataset = dataset[:1000]
+    tree = FPTree(samp_dataset, 7)
     # with open('./fptree.pkl', 'wb') as ftree:
     #     pickle.dump(tree, ftree)
 
@@ -344,20 +354,19 @@ if __name__ == "__main__":
         """
 
         freq_items_ = []
-        for key in sorted([k for k in tree.support_list if k != '_root'], reverse=True):
+        for key in tqdm(sorted([k for k in tree.support_list if k != '_root'], reverse=True)):
             if tree.support_list[key] >= tree.min_support:  # if key is supported
                 freq_items_.append(str(key) + endwith)  # cond_tree is None
-                print(f'key is {tree.support_list[key]}')
+                # print(f'key is {tree.support_list[key]}')
                 tree_cp = deepcopy(tree)
-                # FIXME: tree_cp.item_list's pointer point to nodes from original tree
-                # TODO: a valid way to make a copy
                 # try:
                 cond_tree = tree_cp.cut_tree(key, tree_cp)
                 freq_items_.extend(cond_tree.freq_item(cond_tree, str(key) + endwith))
                 # except KeyError as e:
                 #     print(f'keyerror: {e}')
                 #     return tree_cp
-        return tree  # freq_items_
+        print(freq_items_)
+        return freq_items_
 
 
     fi = freq_item(tree)
